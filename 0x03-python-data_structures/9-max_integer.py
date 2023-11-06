532563817c5a49b351571e0e602a4fd0b6307ce0
Eilla-7 <eilafadel2238@gmail.com>
2023-11-06 11:17:43 -0800

    get the maximum value

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list:
+        max_element = my_list[0]
+        for i in my_list[1:]:
+            if i > max_element:
+                max_element = i
+                return (max_element)
+
+    else:
+        return ("None")
