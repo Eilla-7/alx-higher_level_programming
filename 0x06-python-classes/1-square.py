f75e2803f9058b8c58e82fcfa1ef93f96152eedc
Eilla-7 <eilafadel2238@gmail.com>
2023-11-21 12:52:21 -0800

    size of square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """Define Square"""
+
+    def __init__(self, size):
+        """Constructor
+        Args:
+        size: length of the square
+        """
+        self.__size = size
