2dc1a4af6b5134179077922ead4d35440da8d5b7
Eilla-7 <eilafadel2238@gmail.com>
2023-11-27 19:19:04 -0800

    Queen

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,6 +4,8 @@
 
 
 import sys
+
+
 def init_board(n):
     """Initialize an `n`x`n` sized chessboard with 0's."""
     board = []
@@ -11,11 +13,13 @@ def init_board(n):
     [row.append(' ') for i in range(n) for row in board]
     return (board)
 
+
 def board_deepcopy(board):
     """Return a deepcopy of a chessboard."""
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
     return (board)
+
 
 def get_solution(board):
     """Return the list of lists representation of a solved chessboard."""
@@ -27,44 +31,45 @@ def get_solution(board):
                 break
     return (solution)
 
+
 def xout(board, row, col):
-        """X out spots on a chessboard"""
-        for c in range(col + 1, len(board)):
-            board[row][c] = "x"
-        for c in range(col - 1, -1, -1):
-            board[row][c] = "x"
-        for r in range(row + 1, len(board)):
-            board[r][col] = "x"
-        for r in range(row - 1, -1, -1):
-            board[r][col] = "x"
+    """X out spots on a chessboard"""
+    for c in range(col + 1, len(board)):
+        board[row][c] = "x"
+    for c in range(col - 1, -1, -1):
+        board[row][c] = "x"
+    for r in range(row + 1, len(board)):
+        board[r][col] = "x"
+    for r in range(row - 1, -1, -1):
+        board[r][col] = "x"
 
-        c = col + 1
-        for r in range(row + 1, len(board)):
-            if c >= len(board):
-                break
-            board[r][c] = "x"
-            c += 1
+    c = col + 1
+    for r in range(row + 1, len(board)):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
 
-        c = col - 1
-        for r in range(row - 1, -1, -1):
-            if c < 0:
-                break
-            board[r][c]
-            c -= 1
+    c = col - 1
+    for r in range(row - 1, -1, -1):
+        if c < 0:
+            break
+        board[r][c]
+        c -= 1
 
-        c = col + 1
-        for r in range(row - 1, -1, -1):
-            if c >= len(board):
-                break
-            board[r][c] = "x"
-            c += 1
+    c = col + 1
+    for r in range(row - 1, -1, -1):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
 
-        c = col - 1
-        for r in range(row + 1, len(board)):
-            if c < 0:
-                break
-            board[r][c] = "x"
-            c -= 1
+    c = col - 1
+    for r in range(row + 1, len(board)):
+        if c < 0:
+            break
+        board[r][c] = "x"
+        c -= 1
 
 
 def recursive_solve(board, row, queens, solutions):
@@ -76,8 +81,10 @@ def recursive_solve(board, row, queens, solutions):
             tmp_board = board_deepcopy(board)
             tmp_board[row][c] = "Q"
             xout(tmp_board, row, c)
-            solutions = recursive_solve(tmp_board, row + 1, queens + 1, solutions)
+            solutions = recursive_solve(tmp_board, row + 1,
+                                        queens + 1, solutions)
     return (solutions)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
