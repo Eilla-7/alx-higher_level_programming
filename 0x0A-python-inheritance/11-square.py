60e141ce030e35bc7fa7dd14b2b9b44643d701e4
Eilla-7 <eilafadel2238@gmail.com>
2023-12-06 08:41:36 -0800

    Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -10,6 +10,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
+
     def area(self):
         '''Constructor.'''
         return self.__size ** 2
