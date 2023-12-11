8985f16c6c3d0581a905aa067e04d04516c73a51
Eilla-7 <eilafadel2238@gmail.com>
2023-12-11 08:34:43 -0800

    base class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for Rectangle Class"""
+
+
+class Rectangle(Base):
+    """Representation of Rectangle Class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor of class"""
