70525a2587931bdad18726c7d04d8ecf97dd2a95
Eilla-7 <eilafadel2238@gmail.com>
2023-12-11 14:05:16 -0800

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,3 +8,8 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Constructor"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """String info"""
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
