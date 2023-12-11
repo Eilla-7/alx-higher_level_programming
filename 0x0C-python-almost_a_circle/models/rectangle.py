ef671cd50886a512f3212c79803d3e2bf7cf9349
Eilla-7 <eilafadel2238@gmail.com>
2023-12-11 08:52:52 -0800

    base class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,3 +6,44 @@ class Rectangle(Base):
     """Representation of Rectangle Class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor of class"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """width getter"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """X getter"""
+        return (slef.__x)
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """Y getter"""
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
