d43891104ac8ab88cfa8fdd12caf6483aefefd81
Eilla-7 <eilafadel2238@gmail.com>
2023-12-11 16:48:20 -0800

    File to instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -53,7 +53,7 @@ class Base:
             dummy = None
         dummy.update(**dictionary)
         return dummy
-    
+
     @classmethod
     def load_from_file(cls):
         """returns a list of instances"""
