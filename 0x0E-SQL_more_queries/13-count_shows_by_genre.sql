15a53d17a66276864a534a1fa20ada4d7d377a0d
Eilla-7 <eilafadel2238@gmail.com>
2024-01-17 17:50:09 -0800

    Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,13 @@
--- List genres from hbtn_0d_tvshows with the number of shows linked to each genre
-SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
-FROM tv_genres RIGHT JOIN tv_show_genres
-ON tv_show_genres.id = tv_show_genres.genre_id
-GROUP BY genre
-ORDER BY number_of_shows DESC;
+st genres with the number of shows linked in hbtn_0d_tvshows database
+USE `hbtn_0d_tvshows`; -- Replace with the actual database name
+SELECT
+    tv_genres.name AS genre,
+    COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM
+    tv_genres
+LEFT JOIN
+    tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY
+    genre
+ORDER BY
+    number_of_shows DESC;
