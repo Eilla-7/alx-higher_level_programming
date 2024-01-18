d6ce3acf742850e975e5afd6e5f9269a48518514
Eilla-7 <eilafadel2238@gmail.com>
2024-01-17 16:40:39 -0800

    Create - Read

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create or update user_0d_2 with SELECT privilege in hbtn_0d_2
-CREATE USER 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
 FLUSH PRIVILEGES;
