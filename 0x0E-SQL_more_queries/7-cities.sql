15ace912276b8c849a22f0855ee3e9c693e63755
Eilla-7 <eilafadel2238@gmail.com>
2024-01-17 17:05:00 -0800

    Cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,8 @@
 -- Create or update database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
