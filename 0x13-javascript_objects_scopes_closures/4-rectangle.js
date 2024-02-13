8b41ee984f80b0f7d27c955ce8ebc4af8f3f17ad
Eilla-7 <eilafadel2238@gmail.com>
2024-02-14 02:28:11 +0300

    Rectangle rotate and double

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (h <= 0 || w <= 0) {
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+}
+
+module.exports = Rectangle;
