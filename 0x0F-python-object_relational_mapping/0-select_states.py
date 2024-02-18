5e433ec6d9066f1fc55ec9368132f852ac886629
Eilla-7 <eilafadel2238@gmail.com>
2024-02-19 02:10:55 +0300

    select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,15 +5,15 @@ database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == '__main__':
     """
     Access to the database and get the states
     from the database.
     """
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
@@ -21,6 +21,3 @@ if __name__ == '__main__':
 
     for row in rows:
         print(row)
-
-    cur.close()
-    db.close()
