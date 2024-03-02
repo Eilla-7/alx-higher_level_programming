7dc66b848be496cdaf2e33360feb4ea474b61d0e
Eilla-7 <eilafadel2238@gmail.com>
2024-03-02 23:29:00 +0300

    Request to URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script sends a request to a URL and prints
+information about the response body
+"""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
